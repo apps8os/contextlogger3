195346d28c036659b22bd425bae04d3af98d2175
nalincy <nalincy@gmail.com>
2012-09-30 03:30:44 +0300

    minor changes

--- demoserver/Upload_php/upload.php
+++
@@ -1,5 +0,0 @@
-<?php
- $file=$_FILES["uploadedfile"];
- $file_name=$_FILES["uploadedfile"]['name'];
- move_uploaded_file($_FILES["uploadedfile"]["tmp_name"],"uploads/" . $_FILES["uploadedfile"]["name"]);
-?>
