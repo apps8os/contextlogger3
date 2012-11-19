5ba523bde0dd462e75879a8ff839c3e0e54ddc28
nalincy <nalincy@gmail.com>
2012-11-19 23:02:18 +0200

    Updates in configuration file for Amazon instance (AMI) on Amazon EC2

--- clientframework/gen/org/sizzlelab/contextlogger/android/R.java
+++ clientframework/gen/org/sizzlelab/contextlogger/android/R.java
@@ -17,23 +17,54 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int activity_spinner=0x7f060004;
-        public static final int btn_export=0x7f060009;
-        public static final int btn_toggle=0x7f060002;
-        public static final int btn_toggle_activity=0x7f060005;
-        public static final int dataCountText=0x7f060007;
-        public static final int label=0x7f060000;
-        public static final int label_choose_activity=0x7f060003;
-        public static final int label_start_service=0x7f060001;
-        public static final int lbl_status=0x7f060008;
-        public static final int tv_message=0x7f060006;
+        public static final int activity_spinner=0x7f07000c;
+        public static final int btn_export=0x7f070011;
+        public static final int btn_toggle=0x7f07000a;
+        public static final int btn_toggle_activity=0x7f07000d;
+        public static final int button_start_event=0x7f070014;
+        public static final int context_menu_cancel=0x7f070018;
+        public static final int context_menu_stop=0x7f070017;
+        public static final int dataCountText=0x7f07000f;
+        public static final int edit_text_tag=0x7f070000;
+        public static final int label=0x7f070008;
+        public static final int label_choose_activity=0x7f07000b;
+        public static final int label_start_service=0x7f070009;
+        public static final int lbl_status=0x7f070010;
+        public static final int menu_edit=0x7f070019;
+        public static final int menu_export_data=0x7f07001b;
+        public static final int menu_history=0x7f07001a;
+        public static final int menu_toggle_service=0x7f07001c;
+        public static final int spinner_action_list=0x7f070013;
+        public static final int text_veiw_data_count=0x7f070015;
+        public static final int text_veiw_no_data=0x7f070016;
+        public static final int text_view_event_action_history=0x7f070007;
+        public static final int text_view_event_duration=0x7f070003;
+        public static final int text_view_event_duration_history=0x7f070006;
+        public static final int text_view_event_start_datetime=0x7f070002;
+        public static final int text_view_event_start_datetime_history=0x7f070005;
+        public static final int text_view_event_tag_name=0x7f070001;
+        public static final int text_view_event_tag_name_history=0x7f070004;
+        public static final int text_view_service_state=0x7f070012;
+        public static final int tv_message=0x7f07000e;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add_tag_dialog=0x7f030000;
+        public static final int event_list_item=0x7f030001;
+        public static final int history_list_item=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int main_act=0x7f030004;
+    }
+    public static final class menu {
+        public static final int list_context_menu=0x7f060000;
+        public static final int menu=0x7f060001;
     }
     public static final class string {
         public static final int activity_prompt=0x7f04000c;
+        public static final int add_tag=0x7f040011;
+        public static final int add_tag_hint=0x7f040012;
         public static final int app_name=0x7f040000;
+        public static final int app_quit_content=0x7f040017;
+        public static final int app_quit_title=0x7f040016;
         public static final int btn_export=0x7f04000b;
         public static final int btn_start_activity=0x7f040008;
         public static final int btn_start_service=0x7f040006;
@@ -41,9 +72,16 @@ public final class R {
         public static final int btn_stop_service=0x7f040007;
         public static final int btn_upload=0x7f04000a;
         public static final int choose_activity_instr=0x7f040003;
+        public static final int event_cancel=0x7f04000e;
+        public static final int event_stop=0x7f04000d;
+        public static final int no_data=0x7f040014;
+        public static final int ok=0x7f040015;
+        public static final int save=0x7f040013;
+        public static final int select_activity_instr=0x7f040010;
         public static final int service_running=0x7f040004;
         public static final int service_status=0x7f040001;
         public static final int service_stopped=0x7f040005;
         public static final int start_service_instr=0x7f040002;
+        public static final int view_history=0x7f04000f;
     }
 }
