c5f261b68986dcdb62560a74a8cbfae2e600e2a7
Chao Wei <chao@chao-ubuntu.(none)>
2014-03-30 15:55:03 +0300

    project structure re-design

--- traveldiary/project-code/src/org/sizzlelab/contextlogger/android/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package org.sizzlelab.contextlogger.android.utils;
-
-public interface Constants {
-	public static final String CUSTOM_INTENT_ACTION = "org.sizzlelab.contextlogger.android.customIntentAction";
-	public static final String COUNT_ACTION = "org.sizzlelab.contextlogger.android.totalMessageCount";
-}
